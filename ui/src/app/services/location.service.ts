9a1f31772bb9c3fae5059d8d95c12185f7d171e0
Dmytro Petunenko <d.petunenko@tricentis.com>
2024-07-26 17:15:14 +0200

    add data preparation and displaying line chart

--- ui/src/app/services/location.service.ts
+++ ui/src/app/services/location.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { TimeSeriesData } from '../domain/location';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +18,12 @@ export class LocationService {
         console.error('Error fetching locations', error);
         return of([]);
       })
+    );
+  }
+
+  getTimeSeries(location: string): Observable<TimeSeriesData[]> {
+    return this.http.get<TimeSeriesData[]>(
+      `${this.apiUrl}/${location}/timeseries`
     );
   }
 }
--- ui/src/app/wind-chart/wind-chart.component.ts
+++ ui/src/app/wind-chart/wind-chart.component.ts
@@ -1,14 +1,82 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
+import { Observable, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
+import { TimeSeriesData } from '../domain/location';
+import { LocationService } from '../services/location.service';
 
 @Component({
   selector: 'wind-chart',
   templateUrl: './wind-chart.component.html',
-  styleUrl: './wind-chart.component.scss',
+  styleUrls: ['./wind-chart.component.scss'],
 })
-export class WindChartComponent implements OnInit {
+export class WindChartComponent implements OnChanges {
   @Input() locationName: string = '';
 
-  constructor() {}
+  documentStyle: CSSStyleDeclaration | undefined;
+  timeSeries$: Observable<ChartData<'line'>> = of({ labels: [], datasets: [] });
+  isLoading: boolean = false;
+  error: string | null = null;
 
-  ngOnInit(): void {}
+  chartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    scales: {
+      x: {
+        ticks: {
+          autoSkip: true,
+          autoSkipPadding: 16,
+          maxRotation: 0,
+        },
+      },
+    },
+  };
+
+  constructor(private locationService: LocationService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['locationName'] && this.locationName) {
+      this.loadTimeSeriesData();
+    }
+  }
+
+  // TODO: refactor with extracting transformation functionality to dedicated service
+  private loadTimeSeriesData(): void {
+    this.isLoading = true;
+    this.locationService
+      .getTimeSeries(this.locationName)
+      .pipe(
+        switchMap((data) => {
+          this.isLoading = false;
+          this.error = null;
+          return of({
+            labels: data.map((entry: TimeSeriesData) =>
+              this.formatDate(entry.Time)
+            ),
+            datasets: [
+              {
+                label: 'Wind Gust',
+                data: data.map((entry: TimeSeriesData) => entry.WIND_GUST),
+                borderColor: this.documentStyle?.getPropertyValue('--blue-500'),
+                tension: 0.4,
+                fill: false,
+              },
+            ],
+          });
+        }),
+        catchError((error) => {
+          this.isLoading = false;
+          this.error = 'Error fetching data';
+          return of({ labels: [], datasets: [] });
+        })
+      )
+      .subscribe((data) => {
+        this.timeSeries$ = of(data);
+      });
+  }
+
+  private formatDate(dateString: string): string {
+    const date = new Date(dateString);
+
+    return date.toLocaleDateString();
+  }
 }
