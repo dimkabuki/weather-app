4f7e18ce1c665e00241808606bf6f0043a0536c7
Dmytro Petunenko <petunenkodv@gmail.com>
2024-07-28 23:46:42 +0200

    add warning levels and highlight

--- ui/src/app/wind-chart/wind-chart.component.ts
+++ ui/src/app/wind-chart/wind-chart.component.ts
@@ -1,9 +1,16 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { ChartData, ChartOptions } from 'chart.js';
-import { Observable, of } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
-import { TimeSeriesData } from '../domain/location';
-import { LocationService } from '../services/location.service';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {ChartOptions, ChartTypeRegistry, ScriptableContext} from 'chart.js';
+import {of} from 'rxjs';
+import {catchError, switchMap} from 'rxjs/operators';
+import {TimeSeriesData} from '../domain/location';
+import {LocationService} from '../services/location.service';
+
+const isWarning = (ctx:  ScriptableContext<keyof ChartTypeRegistry>) => {
+    const index = ctx.dataIndex;
+    const windValue = ctx.chart.data.datasets[0].data[index] || 0;
+    const warningValue = ctx.chart.data.datasets[1].data[index] || 0;
+    return warningValue > windValue;
+}
 
 @Component({
   selector: 'wind-chart',
@@ -13,13 +20,18 @@ import { LocationService } from '../services/location.service';
 export class WindChartComponent implements OnChanges {
   @Input() locationName: string = '';
 
-  documentStyle: CSSStyleDeclaration | undefined;
-  timeSeries$: Observable<ChartData<'line'>> = of({ labels: [], datasets: [] });
+  timeSeries$: any = of({ labels: [], datasets: [] });
   isLoading: boolean = false;
   error: string | null = null;
 
-  chartOptions: ChartOptions<'line'> = {
+  chartOptions: ChartOptions = {
     responsive: true,
+    elements: {
+      point: {
+        radius: ctx => isWarning(ctx) ? 4 : 3,
+        backgroundColor: ctx => isWarning(ctx) ? '#c1cffe' : '#00239C'
+      }
+    },
     scales: {
       x: {
         ticks: {
@@ -54,11 +66,17 @@ export class WindChartComponent implements OnChanges {
             ),
             datasets: [
               {
+                type: 'line',
                 label: 'Wind Gust',
                 data: data.map((entry: TimeSeriesData) => entry.WIND_GUST),
-                borderColor: this.documentStyle?.getPropertyValue('--blue-500'),
+                borderColor: '#006EB6',
                 tension: 0.4,
-                fill: false,
+              },
+              {
+                type: 'bar',
+                label: 'Warning',
+                backgroundColor: '#642626',
+                data: data.map((entry: TimeSeriesData) => entry.Warning),
               },
             ],
           });
